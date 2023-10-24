5a7eae11303178355d72288e0bf3a8c594cbc87a
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-10-24 11:11:45 +0530

    #include<stdio.h> int main() {     int n,x;     scanf("%d%d%d", &n, &x);     if(n>=((x*(x+1))/2))     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Sharing_is_Caring.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,x;
+    scanf("%d%d%d", &n, &x);
+    if(n>=((x*(x+1))/2))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
