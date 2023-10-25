3925a562bc97fb5ea1bf79dec0b0f0d42eeda7ac
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-10-25 11:04:36 +0530

    #include<stdio.h> int main() {     int w,x,y,z;     scanf("%d%d%d%d", &w,&x,&y,&z);     if(w==x || w==y || w==z)     {         printf("YES");     }     else if(w==(x+y) || w==(y+z) || w==(z+x) || w==(x+y+z))     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Measuring_the_Object.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int w,x,y,z;
+    scanf("%d%d%d%d", &w,&x,&y,&z);
+    if(w==x || w==y || w==z)
+    {
+        printf("YES");
+    }
+    else if(w==(x+y) || w==(y+z) || w==(z+x) || w==(x+y+z))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
