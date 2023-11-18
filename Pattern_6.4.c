440c5d543ee295cb6ba8e007295c3a0019a0bd8c
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-11-18 21:57:20 +0530

    #include<stdio.h> int main() {     int n;     char c = 'A';     scanf("%d",&n);     for(int i = 1;i<=n;i++)     {         for(int k = 1;k<=(n-i);k++)         {             {                 printf(" ");             }         }         for(int j = 1;j<=i;j++)         {             printf("%c ",c+(n-i));         }         printf(" ");     } }

---
+++ Pattern_6.4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    char c = 'A';
+    scanf("%d",&n);
+    for(int i = 1;i<=n;i++)
+    {
+        for(int k = 1;k<=(n-i);k++)
+        {
+            {
+                printf(" ");
+            }
+        }
+        for(int j = 1;j<=i;j++)
+        {
+            printf("%c ",c+(n-i));
+        }
+        printf("
+");
+    }
+}
