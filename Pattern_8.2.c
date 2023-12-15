147acac3a1c41aa79f1820295810d26c41a7d759
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-15 20:02:55 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2!=0 && n>2)     {         for(int i =0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(j==i || j+i==n-1)                 {                     printf("*");                 }                 else                 {                     printf(" ");                 }             }             printf(" ");         }     } }

---
+++ Pattern_8.2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2!=0 && n>2)
+    {
+        for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==i || j+i==n-1)
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            printf("
+");
+        }
+    }
+}
