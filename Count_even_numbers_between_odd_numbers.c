d71bbfb7431ec84da0a716539e531040a474b40d
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-18 20:41:46 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<n-1;i++)     {         if(arr[i]%2==0)         {             if(arr[i-1]%2!=0 && arr[i+1]%2!=0)             {                 c++;             }         }     }     printf("%d",c); }

---
+++ Count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n-1;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            if(arr[i-1]%2!=0 && arr[i+1]%2!=0)
+            {
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
