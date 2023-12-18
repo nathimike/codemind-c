bd4201e3474092cd36cf3bc2bea5cd884c5b57e5
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-18 21:04:38 +0530

    #include<stdio.h> int main() {     int n,i,c=0,sum=0,avg;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum+=arr[i];         avg=sum/(i+1);     }     for(i=0;i<n;i++)     {         if(arr[i]<=avg)         {             c++;         }     }     printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0,sum=0,avg;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        avg=sum/(i+1);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
