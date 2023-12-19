88a089e8359323f8f6f7ac2ecb8d8bfc90da56dc
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-19 15:02:42 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],arr2[n],c=0,x=0;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2!=0)         {             c++;         }     }     for(int i=0;i<n;i++)     {         for(int j=i;j<n;j++)         {             if(arr[j]%2!=0)             {                 arr2[x]=arr[i];                 x++;                 break;             }             else             {                 arr2[c]=arr[i];                 c++;                 break;             }         }     }     for(int i=0;i<n;i++)     {         printf("%d ",arr2[i]);     } }

---
+++ Array_of_odd_and_even_elements.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],arr2[n],c=0,x=0;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]%2!=0)
+        {
+            c++;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            if(arr[j]%2!=0)
+            {
+                arr2[x]=arr[i];
+                x++;
+                break;
+            }
+            else
+            {
+                arr2[c]=arr[i];
+                c++;
+                break;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr2[i]);
+    }
+}
