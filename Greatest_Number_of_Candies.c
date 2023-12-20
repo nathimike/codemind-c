9a5b92d482457bb5c1e7382df6b4167b9cc4caa8
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-20 15:24:02 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],x,c;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&x);     for(int i=0;i<n;i++)     {         c=0;         for(int j=0;j<n;j++)         {             if(arr[i]+x < arr[j])             {                 c++;             }         }         if(c>0)         {             printf("False ");         }         else         {             printf("True ");         }     } }

---
+++ Greatest_Number_of_Candies.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],x,c;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&x);
+    for(int i=0;i<n;i++)
+    {
+        c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]+x < arr[j])
+            {
+                c++;
+            }
+        }
+        if(c>0)
+        {
+            printf("False ");
+        }
+        else
+        {
+            printf("True ");
+        }
+    }
+}
