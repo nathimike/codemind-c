88b0252362e8b3cd3a59cef48812f9ac6a0e8a92
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-20 13:10:11 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],x,y,flag=0;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&x,&y);     for(int i=0;i<n;i++)     {         if(arr[i]<x || arr[i]>y)         {             flag=1;             printf("%d ",arr[i]);         }     }     if(flag==0)     {         printf("-1");     } }

---
+++ Elements_not_in_between_A_and_B.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],x,y,flag=0;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&x,&y);
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<x || arr[i]>y)
+        {
+            flag=1;
+            printf("%d ",arr[i]);
+        }
+    }
+    if(flag==0)
+    {
+        printf("-1");
+    }
+}
