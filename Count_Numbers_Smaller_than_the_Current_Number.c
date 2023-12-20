4edfa91983cbb20802f93ab99c5a6cdcd1907a3f
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-20 15:52:11 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],c=0;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         c=0;         for(int j=0;j<n;j++)         {             if(arr[i]>arr[j])             {                 c++;             }         }         printf("%d ",c);     } }

---
+++ Count_Numbers_Smaller_than_the_Current_Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],c=0;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                c++;
+            }
+        }
+        printf("%d ",c);
+    }
+}
