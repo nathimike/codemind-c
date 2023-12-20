745addcda1e4cf26fb09f55ffe37020a78e5a59e
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-20 18:04:02 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d%d",&r,&c);     int arr[r][c],sum=0;     for(int i=0;i<r;i++)     {         for(int j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);             sum+=arr[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c],sum=0;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+            sum+=arr[i][j];
+        }
+    }
+    printf("%d",sum);
+}
