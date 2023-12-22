2b3310b7505e49a2fe2f9406eade7b4ef9af7bdf
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-22 11:35:30 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         int c=0;         for(int j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         if(c==1)         {             printf("%d",arr[i]);             break;         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        int c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            printf("%d",arr[i]);
+            break;
+        }
+    }
+}
