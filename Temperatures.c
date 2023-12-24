76fd26ff95a2b2c2b94b42372b4bb575c2238c03
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2023-12-24 11:49:25 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],max=0;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]>max)         {             max=arr[i];         }
        }
        for(int i=0;i<n;i++)
        {
            int c=0,x=0;
            for(int j=i;j<n;j++)
            {
                if(arr[j]>arr[i])
                {
                    break;
                }
                c++;
                if(c==n-i)
                {
                    c=0;
                }
            }
            printf("%d ",c);
        }
    }

---
+++ Temperatures.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],max=0;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+        
+    }
+    for(int i=0;i<n;i++)
+    {
+        int c=0,x=0;
+        for(int j=i;j<n;j++)
+        {
+            if(arr[j]>arr[i])
+            {
+                break;
+            }
+            c++;
+            if(c==n-i)
+            {
+                c=0;
+            }
+        }
+        printf("%d ",c);
+    }
+}
