f7fa484f425ad25e3d7702358d6d768ed887e206
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2024-01-23 18:07:01 +0530

    #include <stdio.h>
    int main() { float a, b; scanf("%f%f", &a, &b); printf("%.f",1/((1/a) + (1/b))); }

--- Water_Taps.c
+++ Water_Taps.c
@@ -1,7 +1,3 @@
 #include <stdio.h>
-int main()
-{
-    float a, b;
-    scanf("%f%f", &a, &b);
-    printf("%.f", 1/((1/a)+(1/b)));
-}
+
+int main() { float a, b; scanf("%f%f", &a, &b); printf("%.f",1/((1/a) + (1/b))); }
