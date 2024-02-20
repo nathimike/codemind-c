249250f5f27da2952e24d44613b20f7d79b674fa
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2024-02-20 21:12:45 +0530

    #include<stdio.h> int main() {     char str[20];     scanf("%s",str);     int v=0,c=0;     for(int i=0;str[i]!=NULL;i++)     {         if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')         {             ++v;             c=0;         }         else         {             v=0;             ++c;         }         if(c==1) printf("C");         else if(v==1) printf("V");     } }

--- Vowel_Consonant_Seqeuence.c
+++ Vowel_Consonant_Seqeuence.c
@@ -1,27 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    char s[20];
-    scanf("%[^
-]",s);
+    char str[20];
+    scanf("%s",str);
     int v=0,c=0;
-    for(int i=0;s[i]!=NULL;i++)
+    for(int i=0;str[i]!=NULL;i++)
     {
-        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
         {
-            v++,c=0;
-            if(v==1)
-            {
-                printf("V");
-            }
+            ++v;
+            c=0;
         }
         else
         {
-            c++,v=0;
-            if(c==1)
-            {
-                printf("C");
-            }
+            v=0;
+            ++c;
         }
+        if(c==1) printf("C");
+        else if(v==1) printf("V");
     }
 }
