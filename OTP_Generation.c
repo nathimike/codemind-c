d9d0d59a79e60a313dbd925581f5654e06bdfbec
Nkosinathi Michael Sibanda <105657102+nathimike@users.noreply.github.com>
2024-02-20 21:49:26 +0530

    #include<stdio.h> int main() {     char str[20];     scanf("%s",str);     int c=0;     for(int i=0;str[i]!=NULL;i++)     {         int x=str[i]-48;         if(x%2==1)         {             printf("%d",x*x);         }     } }

--- OTP_Generation.c
+++ OTP_Generation.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    char s[20];
-    scanf("%[^
-]s",s);
-    for(int i=0;s[i]!=NULL;i++)
+    char str[20];
+    scanf("%s",str);
+    int c=0;
+    for(int i=0;str[i]!=NULL;i++)
     {
-        if(s[i]%2!=0)
+        int x=str[i]-48;
+        if(x%2==1) 
         {
-            int x=s[i]-48;
             printf("%d",x*x);
         }
     }
